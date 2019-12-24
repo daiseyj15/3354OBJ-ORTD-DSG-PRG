1866989e84e6c6584c827b25a0d06a4a4e5866f9
Daisey Jaramillo <53166919+daiseyj15@users.noreply.github.com>
2019-12-24 09:27:35 -0600

    Create Sentiment.java

---
+++ GUI/sentiment/Sentiment.java
@@ -0,0 +1,25 @@
+package sentiment;
+
+
+
+import java.io.File;
+
+
+
+
+
+public class Sentiment {
+
+    public Sentiment(File file) {
+
+    }
+
+
+
+    public int classifyText(String text) {
+
+        return 0;
+
+    }
+
+}
